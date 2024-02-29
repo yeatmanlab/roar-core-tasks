69ea27e4d12b5b8842018d0ff4e9900a31d22498
George Kachergis <george.kachergis@gmail.com>
2024-02-28 17:11:09 -0700

    prepareChoices is now generic and simplified

--- task-launcher/src/tasks/shared/helpers/prepareChoices.js
+++ task-launcher/src/tasks/shared/helpers/prepareChoices.js
@@ -2,33 +2,24 @@ import _shuffle from 'lodash/shuffle'
 import store from "store2";
 import { mediaAssets } from '../../..';
 
-export const prepareChoices = (target, distractors, randomizeOrder=true) => {
-    // randomly select a location for the correct answer
-    console.log({distractors})
-    const randIndex = Math.floor(Math.random() * distractors.length + 1);
-  
-    // randomize the order of the distractors
-    let randomizedChoices;
-    if(randomizeOrder) {
-      randomizedChoices = _shuffle(distractors);
-    } else {
-      randomizedChoices = distractors;
-    }
-  
-    // GK: I don't love this task-specific specification; seems like we should generally
-    // insert the target if it's not already in the distractors
-    if (store.session('config').task !== 'mental-rotation') {
-      // certain tasks like mental rotation have the answer in the distractors 
-      // GK: this is not actually true any longer
-      randomizedChoices.splice(randIndex, 0, target);
-    }
-  
-    store.session.set("target", target);
-    store.session.set("correctResponseIdx", randIndex); // GK: also, isn't this variable meaningless if the target was already in the distractors? 
-    store.session.set("choices", randomizedChoices);
+export const prepareChoices = (target, distractors, randomizeOrder = true) => {
+  console.log({ distractors });
 
-    return {
+  let choices = [target, ...distractors]; // Always include target in the choices
+
+  // Randomize the order of the choices if required
+  if (randomizeOrder) {
+      choices = _shuffle(choices);
+  }
+
+  // Update session variables
+  const correctResponseIdx = choices.indexOf(target); // Find the new index of the target
+  store.session.set("target", target);
+  store.session.set("correctResponseIdx", correctResponseIdx);
+  store.session.set("choices", choices);
+
+  return {
       target: target,
-      choices: randomizedChoices,
-    };
+      choices: choices,
   };
+};
