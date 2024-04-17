88ed0d70f36ba51bb5135da7f514fb0b8509f5cb
Adrien Nabonne <adrien@nabonne.fr>
2024-04-17 16:08:43 -0700

    HaF: pulse animation on practice trials and increase margin above buttons

--- task-launcher/src/tasks/hearts-and-flowers/trials/practice.js
+++ task-launcher/src/tasks/hearts-and-flowers/trials/practice.js
@@ -30,8 +30,32 @@ const practiceData = [
   },
 ];
 
+// TODO: handle the Exception/Error
+function getCorrectInputSide(stimulus, stimulusSide) {
+  if (stimulus === 'heart') {
+    if (stimulusSide === 'left') {
+      return 0;
+    } else if (stimulusSide === 'right') {
+      return 1;
+    } else {
+      console.error('Invalid stimulus side');
+    }
+  } else if (stimulus === 'flower') {
+    if (stimulusSide === 'left') {
+      return 1;
+    } else if (stimulusSide === 'right') {
+      return 0;
+    } else {
+      throw new Error('Invalid stimulus side');
+    }
+  } else {
+    throw new Error('Invalid stimulus');
+  }
+}
+
 const practiceTrials = practiceData.map((data, i) => {
   return {
+
     type: jsPsychHTMLMultiResponse,
     stimulus: () => {
       if (i % 2 === 0) {
@@ -60,40 +84,28 @@ const practiceTrials = practiceData.map((data, i) => {
     },
     on_load: () => {
       document.getElementById('jspsych-html-multi-response-btngroup').classList.add('btn-layout-hf');
+      const buttons = document.querySelectorAll('.response-btn');
+      if (buttons.length != 2) {
+        console.error(`There are ${buttons.length} instead of 2 wrappers in the practice trials`);
+      }
+      const buttonToAnimate = getCorrectInputSide(store.session.get('stimulus'), store.session.get('side'));
+      buttons[buttonToAnimate].style.animation = 'pulse 2s infinite';
     },
     button_choices: ['left', 'right'],
     keyboard_choice: ['ArrowLeft', 'ArrowRight'],
     button_html: [`<button class='response-btn'></button>`, `<button class='response-btn'></button>`],
     on_finish: (data) => {
       // console.log('data in practice: ', data)
-      if (store.session.get('stimulus') === 'heart') {
-        if (data.button_response === 0 || data.button_response === 1) {
-          if (
-            (data.button_response === 0 && store.session.get('side') === 'left') ||
-            (data.button_response === 1 && store.session.get('side') === 'right')
-          ) {
-            store.session.set('correct', true);
-          } else {
-            store.session.set('correct', false);
-          }
+      const expectedCorrectSide = getCorrectInputSide(store.session.get('stimulus'), store.session.get('side'));
+      if (data.button_response === 0 || data.button_response === 1) {
+        if (data.button_response === expectedCorrectSide) {
+          store.session.set('correct', true);
         } else {
-          // Add same logic for keyboard
-          store.session.set('correct', data.keyboard_response);
+          store.session.set('correct', false);
         }
       } else {
-        if (data.button_response === 0 || data.button_response === 1) {
-          if (
-            (data.button_response === 0 && store.session.get('side') === 'right') ||
-            (data.button_response === 1 && store.session.get('side') === 'left')
-          ) {
-            store.session.set('correct', true);
-          } else {
-            store.session.set('correct', false);
-          }
-        } else {
-          // Add same logic for keyboard
-          store.session.set('correct', data.keyboard_response);
-        }
+        // Add same logic for keyboard
+        store.session.set('correct', data.keyboard_response);
       }
     },
   };
