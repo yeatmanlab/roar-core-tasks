97b55c9ed76796fbbc1288c3b2b41b8ff445b40a
Zio-4 <philipz4848@gmail.com>
2024-05-17 16:09:25 -0700

    Removing clearAll call which breaks dashboard

--- task-launcher/src/tasks/shared/trials/finishExperiment.js
+++ task-launcher/src/tasks/shared/trials/finishExperiment.js
@@ -4,7 +4,6 @@ import { jsPsych } from "../../taskSetup";
 
 export function finishExperiment() {
     const t = store.session('translations');
-    store.clearAll()
     jsPsych.endExperiment(
       `<div id="prompt-container-text">
                 <p id="prompt">${t?.taskCompletion || 'Thank you for completing the task! You can now close the window.'}</p>
