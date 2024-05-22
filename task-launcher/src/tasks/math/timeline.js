7512f6ed7059f94a553fb5840df327bb01d9b638
Aritra <aritra@skillprint.co>
2024-05-22 07:19:54 +0530

    make the changes even more minimal

--- task-launcher/src/tasks/math/timeline.js
+++ task-launcher/src/tasks/math/timeline.js
@@ -1,6 +1,5 @@
 import 'regenerator-runtime/runtime';
 import store from 'store2';
-import groupBy from 'lodash/groupBy';
 // setup
 import { getStimulusBlockCount, initTrialSaving, initTimeline, createPreloadTrials } from '../shared/helpers';
 import { jsPsych, initializeCat } from '../taskSetup';
@@ -77,6 +76,12 @@ export default function buildMathTimeline(config, mediaAssets) {
             if (store.session.get('skipCurrentTrial')) {
               store.session.set('skipCurrentTrial', false);
               return false;
+            }
+            const stim = store.session.get('nextStimulus');
+            const skipBlockTrialType = store.page.get('skipCurrentBlock');
+            console.log('mark://', 'conditional Function', {trialType: stim.trialType, skipBlockTrialType});
+            if (stim.trialType === skipBlockTrialType) {
+              return false;
             } else {
               return true;
             }
@@ -92,71 +97,9 @@ export default function buildMathTimeline(config, mediaAssets) {
     }
   };
 
-  const createSubTaskTimeline = (fixationAndSetupBlock, groupedTimeline) => {
-    const tasks = groupedTimeline.keys();
-    tasks.forEach(task => {
-      const stimuli = groupedTimeline.get(task);
-      // This is one block of subtask trials. ex. number-identification
-      const subTaskBlock = {
-        timeline: [],
-        conditional_function: () => {
-          if (store.session.get('skipCurrentBlock')) {
-            store.session.set('skipCurrentBlock', false);
-            return false;
-          } else {
-            return true;
-          }
-        },
-      };
-      stimuli.forEach(f => {
-        let timelineBlock = afcStimulus({ 
-          trialType: 'audio', // or 'html'
-          responseAllowed: true,
-          promptAboveButtons: true,
-          task: config.task,
-        });
-        if (task.includes('Number Line')) {
-          timelineBlock = slider;
-        }
-        // add trials to the block (this is the core procedure for each stimulus)
-        const stimulusBlock = {
-          timeline: [timelineBlock, ifRealTrialResponse],
-          conditional_function: () => {
-            const stim = store.session.get('nextStimulus');
-            const skipBlockTrialType = store.page.get('skipCurrentBlock');
-            console.log('mark://', 'conditional Function', {trialType: stim.trialType, skipBlockTrialType});
-            if (stim.trialType === skipBlockTrialType) {
-              return false;
-            } else {
-              return true;
-            }
-          },
-        };
-        // Pushing in setup seperate so we can conditionally skip the stimulus block
-        subTaskBlock.timeline.push(fixationAndSetupBlock);
-        subTaskBlock.timeline.push(stimulusBlock);
-
-      });
-      timeline.push(subTaskBlock);
-    });
-  };
-
-  const groupedSubTimeLines = new Map();
-  store.session.get('corpora').stimulus.forEach(s => {
-    if (groupedSubTimeLines.has(s.trialType)) {
-      const currentValues = groupedSubTimeLines.get(s.trialType);
-      currentValues.push(s);
-      groupedSubTimeLines.set(s.trialType, currentValues);
-    } else {
-      groupedSubTimeLines.set(s.trialType, [s]);
-    }
-    
-  });
-
   initializeCat();
 
-  // pushSubTaskToTimeline(setupStimulus, getStimulusBlockCount()); // Stimulus Trials
-  createSubTaskTimeline(setupStimulus, groupedSubTimeLines);
+  pushSubTaskToTimeline(setupStimulus, getStimulusBlockCount()); // Stimulus Trials
   timeline.push(taskFinished);
   timeline.push(exitFullscreen);
 
