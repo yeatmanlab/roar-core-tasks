73f76fa1a50009201ce06c36c84e6c1f468e0936
emily-ejag <emily.artegar@gmail.com>
2024-06-17 15:30:36 -0700

    changing databases

--- task-launcher/serve/firebaseConfig.js
+++ task-launcher/serve/firebaseConfig.js
@@ -1,22 +1,35 @@
 // ONLY FOR STANDALONE (WEB APP)
 
 // Change this to use your projects database API keys
+/* eslint-disable import/prefer-default-export */
+
+
+/* eslint-disable import/prefer-default-export */
 const devFirebaseConfig = {
-  apiKey: 'AIzaSyC9IoJBEyN-BxHobeoMQRuEu0CtyQDOg8k',
-  authDomain: 'hs-levante-assessment-dev.firebaseapp.com',
-  projectId: 'hs-levante-assessment-dev',
-  storageBucket: 'hs-levante-assessment-dev.appspot.com',
-  messagingSenderId: '46792247600',
-  appId: '1:46792247600:web:ea20e1fe94e0541dd5a0f5',
+  apiKey: 'AIzaSyCX9WR-j9yv1giYeFsMpbjj2G3p7jNHxIU',
+  authDomain: 'gse-yeatmanlab.firebaseapp.com',
+  projectId: 'gse-yeatmanlab',
+  storageBucket: 'gse-yeatmanlab.appspot.com',
+  messagingSenderId: '292331000426',
+  appId: '1:292331000426:web:91a04220991e3405737013',
+  measurementId: 'G-0TBTMDS993',
 };
 
 const productionFirebaseConfig = {
-  apiKey: 'AIzaSyC9IoJBEyN-BxHobeoMQRuEu0CtyQDOg8k',
-  authDomain: 'hs-levante-assessment-dev.firebaseapp.com',
-  projectId: 'hs-levante-assessment-dev',
-  storageBucket: 'hs-levante-assessment-dev.appspot.com',
-  messagingSenderId: '46792247600',
-  appId: '1:46792247600:web:ea20e1fe94e0541dd5a0f5',
+  apiKey: 'AIzaSyDw0TnTXbvRyoVo5_oa_muhXk9q7783k_g',
+  authDomain: 'gse-roar-assessment.firebaseapp.com',
+  projectId: 'gse-roar-assessment',
+  storageBucket: 'gse-roar-assessment.appspot.com',
+  messagingSenderId: '757277423033',
+  appId: '1:757277423033:web:d6e204ee2dd1047cb77268',
 };
 
+// eslint-disable-next-line no-undef
 export const firebaseConfig = ENV === 'production' ? productionFirebaseConfig : devFirebaseConfig;
+
+export const roarConfig = {
+  firebaseConfig,
+};
+
+// eslint-disable-next-line operator-linebreak
+const logMessage = `This ROAR app will write data to the ${roarConfig.firebaseConfig.projectId} firestore database`;
