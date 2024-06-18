f946a411dcb5853dedc40621ba6148c3e3dde304
emily-ejag <emily.artegar@gmail.com>
2024-06-18 10:46:55 -0700

    integrating cypress repo key

--- task-launcher/cypress.config.js
+++ task-launcher/cypress.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  projectId: 'g63x9c',
   e2e: {
+    experimentalRunAllSpecs: true,
+    retries: 2,
+    // eslint-disable-next-line no-unused-vars
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+    env: {
+      baseUrl: process.env.CYPRESS_BASE_URL ?? 'http://localhost:8000',
+      timeout: 10000,
     },
   },
 });
